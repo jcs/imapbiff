d6c2024b7708a333b4913b02c7ead3f8997628bb
jcs <jcs>
2008-03-03 02:41:54 +0000

    dbus support

--- imapbiff.pl
+++ imapbiff.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
-# $Id: imapbiff.pl,v 1.6 2006/12/16 02:14:01 jcs Exp $
+# $Id: imapbiff.pl,v 1.7 2008/03/03 02:41:54 jcs Exp $
 #
-# imap biff with growl/meow notification
+# imap biff with meow/growl/dbus notification
 #
-# Copyright (c) 2006 joshua stein <jcs@jcs.org>
+# Copyright (c) 2006, 2008 joshua stein <jcs@jcs.org>
 # ssl work-around code from Nick Burch (http://gagravarr.org/code/)
 #
 # Redistribution and use in source and binary forms, with or without
@@ -52,25 +52,28 @@
 #	# for verbosity
 #	#$debug = 1;
 #
-#	# define on mac os x to use growl
-#	#$growl = 1;
+#	# which notification mechanism to use ("meow", "dbus", or "growl")
+#	$notify = "dbus";
 #
 
 use strict;
 use Fcntl;
-use Mail::IMAPClient;
 use IO::Socket::SSL;
+use Mail::IMAPClient;
+use MIME::Base64;
+use MIME::QuotedPrint;
 
-my (%config, $debug, $growl, $sleepint, $socktimeout);
+my (%config, $debug, $notify, $icon, $sleepint, $socktimeout);
+our ($dbus, $dbus_service, $dbus_notification);
 
-# default sleep between check intervals is 120 seconds
-$sleepint = 30;
+# default seconds to sleep between check intervals
+$sleepint = 60;
 
 # seconds to allow a folder check to take
 $socktimeout = 10;
 
-# assume we're not on mac os x
-$growl = 0;
+# use dbus notifications by default
+$notify = "dbus";
 
 # read the user's config
 my $configdat;
@@ -83,6 +86,27 @@ close(C);
 
 eval($configdat) or die "invalid configuration file, exiting\n";
 
+# check for bogus config
+if ($notify != "dbus" || $notify != "meow" || $notify != "growl") {
+	die "unknown notification type \"" . $notify . "\"\n";
+}
+
+if ($notify eq "dbus") {
+	use Net::DBus;
+
+	$dbus = Net::DBus->find;
+	$dbus_service = $dbus->get_service("org.freedesktop.Notifications");
+	$dbus_notification = $dbus_service->get_object(
+		"/org/freedesktop/Notifications", "org.freedesktop.Notifications");
+}
+
+# clean up nicely if we can
+$SIG{"TERM"} = "die_signal_handler";
+$SIG{"INT"} = "die_signal_handler";
+
+# create tmp file with icon to pass to notifiers
+$icon = write_icon();
+
 # init, build connections
 foreach my $server (keys %config) {
 	foreach my $folder (@{$config{$server}{"folders"}}) {
@@ -91,6 +115,8 @@ foreach my $server (keys %config) {
 
 	imap_connect($server);
 }
+
+notify("imapbiff", "imapbiff started");
 
 # run forever
 for(;;) {
@@ -133,6 +159,8 @@ for(;;) {
 				if ($debug) {
 					print "server connection timed out: " . $@ . "\n";
 				}
+
+				notify("imapbiff", "lost connection to server " . $server);
 
 				# throttle
 				sleep $sleepint;
@@ -243,26 +271,120 @@ sub announce_message {
 			. $server . "\n";
 	}
 
-	my $subject = $$imap->get_header($msgno, "Subject");
-	my $from = $$imap->get_header($msgno, "From");
+	my $subject = hsc(decode_qp($$imap->get_header($msgno, "Subject")));
+	my $from = hsc(decode_qp($$imap->get_header($msgno, "From")));
 
 	# strip high ascii because growlnotify likes to segfault on it
 	$subject =~ tr/\000-\177//cd; 
 	$from =~ tr/\000-\177//cd; 
 
-	if ($growl) {
+	$folder =~ tr/[A-Z]/[a-z]/;
+
+	if ($notify eq "growl") {
+		notify($subject, "From: " . $from);
+	}
+
+	elsif ($notify eq "meow") {
+		notify("New message in <b>" . $folder . "</b>\n"
+			. "From <b>" . $from . "</b>:\n"
+			. $subject);
+	}
+
+	elsif ($notify eq "dbus") {
+		notify("[" . $folder . "] " . $subject, "From " . $from);
+	}
+}
+
+sub notify {
+	if ($notify eq "growlnotify") {
 		system("/usr/local/bin/growlnotify",
 			"-n", "imapbiff",
-			"--image", "/Applications/Mail.app/Contents/Resources/drag.tiff",
-			"-t", $subject,
-			"-m", "From " . $from);
-	} else {
+			"--image", $icon,
+			"-t", $_[0],
+			"-m", $_[1]);
+	}
+
+	elsif ($notify eq "meow") {
 		sysopen(MEOW, ($ENV{"MEOW_SOCK"} ? $ENV{"MEOW_SOCK"}
 			: $ENV{"HOME"} . "/.meow"), O_WRONLY|O_NONBLOCK) or
 			die "can't write to meow socket (not running?)";
-		print MEOW "||" . "New message in <b>" . $folder . "</b>\n"
-			. "From <b>" . $from . "</b>:\n"
-			. $subject . "\n";
+
+		print MEOW "||" . $_[0] . "\n";
+
 		close(MEOW);
 	}
+	
+	elsif ($notify eq "dbus") {
+		$dbus_notification->Notify(
+			"imapbiff",
+			0,
+			$icon,
+			$_[0],
+			$_[1],
+			[],
+			{},
+			-1
+		);
+	}
+}
+
+sub hsc {
+	my $text = $_[0];
+
+	$text =~ s/&/&amp;/g;
+	$text =~ s/</&lt;/g;
+	$text =~ s/>/&gt;/g;
+
+	return $text;
+}
+
+sub die_signal_handler {
+	if ($debug) {
+		print "got deadly signal, deleting " . $icon . "\n";
+	}
+
+	unlink($icon);
+
+	exit;
+}
+
+sub write_icon {
+	chop(my $tmp = `mktemp /tmp/imapbiff.XXXXXX`);
+	if (!$tmp) {
+		die "can't make temp file: " . $1;
+	}
+
+	open(TMP, ">>" . $tmp) or die "can't write to temp file " . $tmp . ":"
+		. $!;
+	print TMP MIME::Base64::decode(
+'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAAZiS0dEAP8A
+/wD/oL2nkwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9gCFREbBwUIV1EAAAUhSURBVFjD
+7ZdpbFRVFMd/s3Zm2s5Gp2WmLUvbCKRYKFQImwUES2WLEQtfwDTsCUIgUTRiIjFxTQh+ElGMCUoM
+ESzE1C2hRMACKaW2RSPttAKdIVJKZ6bLLG/evX4oVJAiQwt+8iQ3ee/lnv8573/+75z74H8bglVV
+VZmGiqEdinNbUFn/7r6De4eCoRmMU01NzYhjv3hf1xtNL4xwpxu8rZc+3r52xbrb91RWViaFNClv
+/HZ61ms7d2rEQ2Wgptm32mazl29a/ozh2TlTyBmVvfbtj77oZ6LiWPWWan/kakdEvDJ74fm5D42B
+s2cbss81t27QGozlS+dMcXf1amm+HGTuE8OpqDpDVyj0bUdMjsjJysxfPGMCnd297K880Th3TPrM
+oqKi4ECY+gdKoMm7yWxJfnl2UQHetijfnLhCTBFoiWO2WAgolJaXFOGymqn1waTMFEZnZoz3BaLl
+wO5BM3Dw4EFjty5layisvPj8/BmZvREdn1d66Q7HycpQiWpCLJxWyPhRGbQG4euqMN2h65TOyCY/
+S+H9A5WdSwtHPl5YWOh7YAZqa2tdP9T+/pbFItYsmzedNHsq4WgcW6qKNuk64x7LoaRoOgA3orC/
+4gqxaA9JphSOn2sne5iL4onjHA0+3w5g4wOLsK7l6kqj0bxi1aJiXA4rgWAXR06eIz1dsmnZXEqK
+xvXvTTWAzaJiTrYTj0W4dvUSFy93MbMgj6uh6IYTp2snJFyC6vPnR52u926WGn35yoXFdnOSkVP1
+F/m1rZ2SqRMZN8LVvzfUE0an05GUZMTri/LJoQbMJh1PT8tm8lg7UkoaWnz8XHfh+yx975KysrLY
+fUtQ09i60Wa1bV00azKt/mtUVDfyZOFYVi+eTarJgAqoErq6euns7EAIgcPhxGqKM63AxaxJaWg1
+0BuJApDrSaO60Viid9hLgSP/ysCeA0d3xKRcP71gTNaP55vwpKcxe+IY7A47QgiEEKiqihCCcDhM
+T3c3qhrHbneg1f5dVSnlHbjtgS4OHTvTPHG0dWrZggU37slAOBZ902ixcrzxD56aOoHhzj7gQLC7
+LwEp+sClRAiBVm9AbzQRVVSkjN9TTylmE1kZaXkdoega4L27GNj12WH5Xw2yLGfqyLIl8y/3M/DO
+p4dfdWeksWLBrEce/MvvTlLX3PYhsFAP8NIH+ysxmEoVVSIl+Hw+/H4/ubm5eL1enE4nubm5gwr2
+3PaG/mt7Rj77tmlRVEFEUVL6NWAzG4929YbnxW3JBiklUkpycnKQUuJ2u9FoNHcJKlGzZ+TfcS+l
+JC5g17ZVxf2NaMe65XtudN6oVgQIKXF7PNgdDuwOB26Ph+FuN0LKQa3Anxf6F/ThK2KAVuy2JW2O
+S02dkOD3+TCZzQC0tHjxuD2YzGacTueQGRASVDFwJ9Ts/uonsX7J9Ecuwr1HT7FlWbHmn31AKkIi
+xf1r3dTURLrLhd5gwO/3k5xsAcDjyUwogZjQDDwNYyqoCYgtJy9vwGs1QaHGVDlwAoogIbUHgwEU
+RSHQGcCV7uJ6+3Vy8/Lo7enBkpx8X39F3CuBuERNoASqkEQiMUxmCx0dnTiGDSMSjaGKxPxj6j0O
+JDGZ2PdutdqwWm13Pb/VQ+7PwN0a0AC6uCoRQ5gGOr0hIf94nwYMQFx/28nIEmfwHe9BLN7X/yxA
+j/7m2+sB84X6OpbX1/1XA9ECRDQ3EzACNiAF0A32jylBk4AK9ADBW4F0N5N41MHvrATE/gLYd1mH
+8lBaPAAAAABJRU5ErkJggg=='
+	);
+	close(TMP);
+
+	return $tmp;
 }
